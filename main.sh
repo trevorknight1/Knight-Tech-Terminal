8eb3a66d9308b3c01ea016a119da0b930ca42d97
trevorknight1 <46654836+trevorknight1@users.noreply.github.com>
2022-02-19 18:20:06 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,24 +9,24 @@ bind '"\e[B": "cache'
 SOURCE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 clear
 # -----------------GUI FILES---------------------
-HEADER="python $SOURCE/terminal/gui/header.py"
-FOOTER="python $SOURCE/terminal/gui/exit_footer.py"
-MENU="python $SOURCE/terminal/menu.py"
+HEADER="python3 $SOURCE/terminal/gui/header.py"
+FOOTER="python3 $SOURCE/terminal/gui/exit_footer.py"
+MENU="python3 $SOURCE/terminal/menu.py"
 # -----------------------------------------------
 # ----------------JUMPER FILES-------------------
-JUMPER="python $SOURCE/terminal/jumper/jumper.py"
-CREATE_JUMP="python $SOURCE/terminal/jumper/new_jumpsite.py"
+JUMPER="python3 $SOURCE/terminal/jumper/jumper.py"
+CREATE_JUMP="python3 $SOURCE/terminal/jumper/new_jumpsite.py"
 # -----------------------------------------------
 # -----------------CACHE FILES-------------------
-CMD="python $SOURCE/terminal/cache/command_cache.py"
+CMD="python3 $SOURCE/terminal/cache/command_cache.py"
 # -----------------------------------------------
 # ----------------ALIAS FILES-------------------
-ALIAS="python $SOURCE/terminal/alias/alias.py"
-CREATE_ALIAS="python $SOURCE/terminal/alias/new_alias.py"
+ALIAS="python3 $SOURCE/terminal/alias/alias.py"
+CREATE_ALIAS="python3 $SOURCE/terminal/alias/new_alias.py"
 # -----------------------------------------------
 # ----------------COMPUTER FILES-------------------
-COMPUTER="python $SOURCE/terminal/computers/computer.py"
-CREATE_COMPUTER="python $SOURCE/terminal/computers/new_computer.py"
+COMPUTER="python3 $SOURCE/terminal/computers/computer.py"
+CREATE_COMPUTER="python3 $SOURCE/terminal/computers/new_computer.py"
 # -----------------------------------------------
 $HEADER
 while [ "$varname" != "quit" ]
@@ -34,12 +34,12 @@ while [ "$varname" != "quit" ]
             # Start Up Functionality -----------------
             printf "${RED}Knight Tech ${Yellow}>>${NC}"
             read -ep " " varname 
-            VARIABLE=`python $SOURCE/terminal/cmd_op/iscd.py $varname`
-            isalias=`python $SOURCE/terminal/alias/isalias.py $varname`
+            VARIABLE=`python3 $SOURCE/terminal/cmd_op/iscd.py $varname`
+            isalias=`python3 $SOURCE/terminal/alias/isalias.py $varname`
             # ----------------------------------------
             if [ "$varname" == "cache" ]; then
                   $CMD 
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
 
             elif [ "$VARIABLE" == "True" ]; then
                   $varname
@@ -48,14 +48,14 @@ while [ "$varname" != "quit" ]
                   $FOOTER
             elif [ "$varname" == "jumper" ]; then
                   $JUMPER
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
                   pwd
             elif [ "$varname" == "computers" ]; then
                   $COMPUTER
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
             elif [ "$varname" == "create computer" ]; then
                   $CREATE_COMPUTER
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
             elif [ "$varname" == "menu" ]; then
                   $MENU
             elif [ "$varname" == "create jumper" ]; then
@@ -64,13 +64,13 @@ while [ "$varname" != "quit" ]
                   $CREATE_ALIAS
             elif [ "$varname" == "alias" ]; then
                   $ALIAS
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
             elif [ "$varname" == "help" ]; then
                   echo "create jumper jumper"
             elif [ "$isalias" == "True" ]; then
-                  `python $SOURCE/terminal/cmd_op/get_cmd.py`
+                  `python3 $SOURCE/terminal/cmd_op/get_cmd.py`
             else
-                  python $SOURCE/terminal/cache/create_command_cache.py $varname
+                  python3 $SOURCE/terminal/cache/create_command_cache.py $varname
                   $varname
             fi
       done
