31a62c4a8e429c0b717fe252f7e1af172ff9f218
trevorknight1 <46654836+trevorknight1@users.noreply.github.com>
2022-02-20 17:49:48 -0500

    Update movefile.py

--- terminal/movefile/movefile.py
+++ terminal/movefile/movefile.py
@@ -21,11 +21,11 @@ def main():
       select = term.green("Select File To Move >> " )
       input_f = input(select)
       if os.path.isfile(os.path.join(os.getcwd(), input_f)):
-            notice = term.yellow("File Selected")
+            notice = term.yellow("File Selected: os.path.abspath(os.path.join(os.getcwd(), input_f))")
       elif os.path.isdir(os.path.join(os.getcwd(), input_f)):
-            notice = term.yellow("Directory Selected")
+            notice = term.yellow("Directory Selected : " + os.path.abspath(os.path.join(os.getcwd(), input_f)))
       else:
-           notice =  term.yellow("not a directory or file")
+           notice =  term.yellow("not a directory or file: " + os.path.abspath(os.path.join(os.getcwd(), input_f)))
            
       print(notice)
       print(os.path.abspath(os.path.join(os.getcwd(), input_f)))
